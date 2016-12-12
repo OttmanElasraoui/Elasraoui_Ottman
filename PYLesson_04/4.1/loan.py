32823f308c9ffdb8d8ae2d0ab8f631c9f110c115
OttmanElasraoui <bobottman@gmail.com>
2016-12-12 10:12:43 -0800

    CHANGE
    C-ANGE

---
+++ PYLesson_04/4.1/loan.py
@@ -0,0 +1,12 @@
+R = float(input("Enter the intrest rate"))
+P = float(input("Enter the principle amount"))
+N = float(input("Enter the amount of times its compounded per year"))
+T = float(input("Enter the number of years"))
+
+
+def calcVol()(word, number)
+        global A
+    A = (P*(1+R/N)**(N*T))/(T*12)
+    print("Your final amount is," , A , "Correct?")
+
+calcVol() printf(word, number)
