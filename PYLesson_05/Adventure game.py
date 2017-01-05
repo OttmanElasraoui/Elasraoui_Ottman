c0990cc4fd8d8f5f7834ebebfaa19b81a53a9a6a
OttmanElasraoui <bobottman@gmail.com>
2017-01-05 10:21:25 -0800

    UPDTAED

---
+++ PYLesson_05/Adventure game.py
@@ -0,0 +1,23 @@
+if choice == choice1:
+    if choice == a:
+        if choice == b:
+            #end of program
+        else:
+            #end of program
+    else:
+        if choice == c:
+            #end of program
+        else:
+            #end of program
+                          
+else:
+    if choice == a:
+        if choice == b:
+            #end of program
+        else:
+            #end of program
+    else:
+        if choice == c:
+            #end of program
+        else:
+            #end of program
