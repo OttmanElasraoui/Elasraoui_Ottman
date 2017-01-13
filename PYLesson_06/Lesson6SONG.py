71be045ec4ad80bc38a43e222423f00ccaf60764
OttmanElasraoui <bobottman@gmail.com>
2017-01-13 12:48:21 -0800

    updated

---
+++ PYLesson_06/Lesson6SONG.py
@@ -0,0 +1,10 @@
+def sing (word, times):
+    output = ""
+    for i in range(0, times):
+        output += word + " "
+    print(output)
+
+sing("Na", 4)
+sing("Na", 4)
+sing("Hey", 3)
+sing("Goodbye!", 1)
